c549badb0a40352e071699142d4528c1f7a14424
Joey Yandle <xoloki@gmail.com>
2025-08-21 20:09:50 -0400

    prepare to implement ops by making q a struct field

--- src/lib.rs
+++ src/lib.rs
@@ -18,136 +18,87 @@ fn min_dist(a: i64, b: i64, q: i64) -> i64 {
 pub struct Polynomial {
     pub coeffs: Vec<i64>,
     n: usize,
+    q: i64,
 }
 
 impl Polynomial {
-    fn new(n: usize) -> Self {
+    fn new(n: usize, q: i64) -> Self {
         Polynomial {
             coeffs: vec![0i64; n],
             n,
+            q,
         }
     }
 
-    fn add(&self, other: &Self, q: i64) -> Self {
-        let mut res = Self::new(self.n);
+    fn add(&self, other: &Self) -> Self {
+        let mut res = Self::new(self.n, self.q);
         for i in 0..self.n {
-            res.coeffs[i] = modq(self.coeffs[i] + other.coeffs[i], q);
+            res.coeffs[i] = modq(self.coeffs[i] + other.coeffs[i], self.q);
         }
         res
     }
 
-    fn neg(&self, q: i64) -> Self {
-        let mut res = Self::new(self.n);
+    fn neg(&self) -> Self {
+        let mut res = Self::new(self.n, self.q);
         for i in 0..self.n {
-            res.coeffs[i] = modq(-self.coeffs[i], q);
+            res.coeffs[i] = modq(-self.coeffs[i], self.q);
         }
         res
     }
 
-    fn mul(&self, other: &Self, q: i64) -> Self {
+    fn mul(&self, other: &Self) -> Self {
         let mut res_coeffs = vec![0i64; 2 * self.n - 1];
         for i in 0..self.n {
             for j in 0..self.n {
                 res_coeffs[i + j] = modq(
                     res_coeffs[i + j] + self.coeffs[i] * other.coeffs[j],
-                    q,
+                    self.q,
                 );
             }
         }
         // Reduce mod x^n + 1
         for i in (self.n..(2 * self.n - 1)).rev() {
-            res_coeffs[i - self.n] = modq(res_coeffs[i - self.n] - res_coeffs[i], q);
+            res_coeffs[i - self.n] = modq(res_coeffs[i - self.n] - res_coeffs[i], self.q);
         }
-        let mut res = Self::new(self.n);
+        let mut res = Self::new(self.n, self.q);
         for i in 0..self.n {
-            res.coeffs[i] = modq(res_coeffs[i], q);
-        }
-        res
-    }
-}
-/*
-impl Add<Polynomial> for Polynomial {
-    type Output = Self;
-
-    fn add(self, other: Polynomial) -> Self::Output {
-        let mut res = Self::new(self.n);
-        for i in 0..self.n {
-            res.coeffs[i] = self.coeffs[i] + other.coeffs[i];
+            res.coeffs[i] = modq(res_coeffs[i], self.q);
         }
         res
     }
 }
 
-impl Neg<Polynomial> for Polynomial {
-    type Output = Self;
-    
-    fn neg(self) -> Self::Output {
-        let mut res = Self::new(self.n);
-        for i in 0..self.n {
-            res.coeffs[i] = -self.coeffs[i];
-        }
-        res
-    }
-}
-
-impl Mul<Polynomial> for Polynomial {
-    type Output = Self;
-    
-    fn mul(self, other: Polynomial) -> Self::Output {
-        let mut res_coeffs = vec![0i64; 2 * self.n - 1];
-        for i in 0..self.n {
-            for j in 0..self.n {
-                res_coeffs[i + j] = modq(
-                    res_coeffs[i + j] + self.coeffs[i] * other.coeffs[j],
-                    q,
-                );
-            }
-        }
-        // Reduce mod x^n + 1
-        for i in (self.n..(2 * self.n - 1)).rev() {
-            res_coeffs[i - self.n] = modq(res_coeffs[i - self.n] - res_coeffs[i], q);
-        }
-        let mut res = Self::new(self.n);
-        for i in 0..self.n {
-            res.coeffs[i] = res_coeffs[i];
-        }
-        res
-    }
-}
-
-impl Rem<i64> for Polynomial {
-    type Output = Self;
-}
-*/
 #[derive(Clone)]
 /// A vector of polynomials
 pub struct Vector {
     polys: Vec<Polynomial>,
     d: usize,
     n: usize,
+    q: i64,
 }
 
 impl Vector {
-    fn new(d: usize, n: usize) -> Self {
+    fn new(d: usize, n: usize, q: i64) -> Self {
         Vector {
-            polys: vec![Polynomial::new(n); d],
+            polys: vec![Polynomial::new(n, q); d],
             d,
             n,
+            q,
         }
     }
 
-    fn add(&self, other: &Self, q: i64) -> Self {
-        let mut res = Self::new(self.d, self.n);
+    fn add(&self, other: &Self) -> Self {
+        let mut res = Self::new(self.d, self.n, self.q);
         for i in 0..self.d {
-            res.polys[i] = self.polys[i].add(&other.polys[i], q);
+            res.polys[i] = self.polys[i].add(&other.polys[i]);
         }
         res
     }
 
-    fn inner(&self, other: &Self, q: i64) -> Polynomial {
-        let mut res = Polynomial::new(self.n);
+    fn inner(&self, other: &Self) -> Polynomial {
+        let mut res = Polynomial::new(self.n, self.q);
         for i in 0..self.d {
-            res = res.add(&self.polys[i].mul(&other.polys[i], q), q);
+            res = res.add(&self.polys[i].mul(&other.polys[i]));
         }
         res
     }
@@ -159,24 +110,26 @@ pub struct Matrix {
     rows: Vec<Vec<Polynomial>>,
     d: usize,
     n: usize,
+    q: i64,
 }
 
 impl Matrix {
-    fn new(d: usize, n: usize) -> Self {
+    fn new(d: usize, n: usize, q: i64) -> Self {
         Matrix {
-            rows: vec![vec![Polynomial::new(n); d]; d],
+            rows: vec![vec![Polynomial::new(n, q); d]; d],
             d,
             n,
+            q,
         }
     }
 
     /// Computes A * v.
-    fn matmul(&self, v: &Vector, q: i64) -> Vector {
-        let mut res = Vector::new(self.d, self.n);
+    fn matmul(&self, v: &Vector) -> Vector {
+        let mut res = Vector::new(self.d, self.n, self.q);
         for i in 0..self.d {
-            let mut sum = Polynomial::new(self.n);
+            let mut sum = Polynomial::new(self.n, self.q);
             for j in 0..self.d {
-                sum = sum.add(&self.rows[i][j].mul(&v.polys[j], q), q);
+                sum = sum.add(&self.rows[i][j].mul(&v.polys[j]));
             }
             res.polys[i] = sum;
         }
@@ -184,12 +137,12 @@ impl Matrix {
     }
 
     /// Computes A^T * v.
-    fn matmul_transposed(&self, v: &Vector, q: i64) -> Vector {
-        let mut res = Vector::new(self.d, self.n);
+    fn matmul_transposed(&self, v: &Vector) -> Vector {
+        let mut res = Vector::new(self.d, self.n, self.q);
         for j in 0..self.d {
-            let mut sum = Polynomial::new(self.n);
+            let mut sum = Polynomial::new(self.n, self.q);
             for i in 0..self.d {
-                sum = sum.add(&self.rows[i][j].mul(&v.polys[i], q), q);
+                sum = sum.add(&self.rows[i][j].mul(&v.polys[i]));
             }
             res.polys[j] = sum;
         }
@@ -210,7 +163,7 @@ pub struct Params {
 }
 
 fn sample_uniform_poly<R: Rng>(rng: &mut R, n: usize, q: i64) -> Polynomial {
-    let mut p = Polynomial::new(n);
+    let mut p = Polynomial::new(n, q);
     for i in 0..n {
         p.coeffs[i] = rng.gen_range(0..q);
     }
@@ -218,7 +171,7 @@ fn sample_uniform_poly<R: Rng>(rng: &mut R, n: usize, q: i64) -> Polynomial {
 }
 
 fn sample_small_poly<R: Rng>(rng: &mut R, n: usize, eta: i64, q: i64) -> Polynomial {
-    let mut p = Polynomial::new(n);
+    let mut p = Polynomial::new(n, q);
     for i in 0..n {
         let c: i64 = rng.gen_range(-eta..=eta);
         p.coeffs[i] = modq(c, q);
@@ -226,8 +179,8 @@ fn sample_small_poly<R: Rng>(rng: &mut R, n: usize, eta: i64, q: i64) -> Polynom
     p
 }
 
-pub fn sample_binary_poly<R: Rng>(rng: &mut R, n: usize, _q: i64) -> Polynomial {
-    let mut p = Polynomial::new(n);
+pub fn sample_binary_poly<R: Rng>(rng: &mut R, n: usize, q: i64) -> Polynomial {
+    let mut p = Polynomial::new(n, q);
     for i in 0..n {
         p.coeffs[i] = rng.gen_range(0..=1);
     }
@@ -235,7 +188,7 @@ pub fn sample_binary_poly<R: Rng>(rng: &mut R, n: usize, _q: i64) -> Polynomial 
 }
 
 fn sample_uniform_matrix<R: Rng>(rng: &mut R, d: usize, n: usize, q: i64) -> Matrix {
-    let mut mat = Matrix::new(d, n);
+    let mut mat = Matrix::new(d, n, q);
     for i in 0..d {
         for j in 0..d {
             mat.rows[i][j] = sample_uniform_poly(rng, n, q);
@@ -245,7 +198,7 @@ fn sample_uniform_matrix<R: Rng>(rng: &mut R, d: usize, n: usize, q: i64) -> Mat
 }
 
 fn sample_small_vec<R: Rng>(rng: &mut R, d: usize, n: usize, eta: i64, q: i64) -> Vector {
-    let mut v = Vector::new(d, n);
+    let mut v = Vector::new(d, n, q);
     for i in 0..d {
         v.polys[i] = sample_small_poly(rng, n, eta, q);
     }
@@ -257,7 +210,7 @@ pub fn keygen<R: Rng>(params: &Params, rng: &mut R) -> ((Matrix, Vector), Vector
     let a = sample_uniform_matrix(rng, params.d, params.n, params.q);
     let s = sample_small_vec(rng, params.d, params.n, params.eta, params.q);
     let e = sample_small_vec(rng, params.d, params.n, params.eta, params.q);
-    let t = a.matmul(&s, params.q).add(&e, params.q);
+    let t = a.matmul(&s).add(&e);
     ((a, t), s)
 }
 
@@ -272,29 +225,29 @@ pub fn encrypt<R: Rng>(
     let r = sample_small_vec(rng, params.d, params.n, params.eta, params.q);
     let e1 = sample_small_vec(rng, params.d, params.n, params.eta, params.q);
     // u = A*r + e1 (mod q)
-    let u = a.matmul_transposed(&r, params.q).add(&e1, params.q);
+    let u = a.matmul_transposed(&r).add(&e1);
     let e2 = sample_small_poly(rng, params.n, params.eta, params.q);
     // <t, r> (mod q)
-    let inner = t.inner(&r, params.q);
+    let inner = t.inner(&r);
     // Scale message by floor(q/2) to encode
-    let mut m_scaled = Polynomial::new(params.n);
+    let mut m_scaled = Polynomial::new(params.n, params.q);
     let scale = params.q / 2;
     for i in 0..params.n {
         m_scaled.coeffs[i] = modq(m.coeffs[i] * scale, params.q);
     }
     // v = <t, r> + m (mod q) [scaled to q/2]
-    let v = inner.add(&e2, params.q).add(&m_scaled, params.q);
+    let v = inner.add(&e2).add(&m_scaled);
     (u, v)
 }
 
 /// Decrypts the ciphertext (u, v) using secret key s. Returns the message polynomial.
 pub fn decrypt(params: &Params, s: &Vector, u: &Vector, v: &Polynomial) -> Polynomial {
     // <u, s> (mod q)
-    let inner = u.inner(s, params.q);
+    let inner = u.inner(s);
     // v - <u, s> (mod q)
-    let approx = v.add(&inner.neg(params.q), params.q);
+    let approx = v.add(&inner.neg());
     // Decode each coefficient by rounding to nearest multiple of floor(q/2)
-    let mut m = Polynomial::new(params.n);
+    let mut m = Polynomial::new(params.n, params.q);
     let q2 = params.q / 2;
     for i in 0..params.n {
         let c = approx.coeffs[i];
@@ -373,11 +326,12 @@ mod tests {
         let params = Params {
             n: 256,
             d: 2,
-            q: 101, // Increased q for more noise tolerance
+            q: 65535, // Increased q for more noise tolerance
             eta: 1,
         };
         let trials = 100;
         let failures = test_encryption(&params, trials, &mut rng);
+        assert_eq!(failures, 0);
         println!("Failures: {}/{}", failures, trials);
     }
 }
